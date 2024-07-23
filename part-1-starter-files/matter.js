cb191089bf74e0e6e12652b3ef70868da81364c3
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2024-07-23 22:41:11 +0530

    Update matter.js

--- part-1-starter-files/matter.js
+++ part-1-starter-files/matter.js
@@ -1,3 +1,9 @@
+{/* <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.12.0/matter.min.js"></script>
+<script src="https://cdn/jsdelivr.net/npm/matter-wrap@0.2.0/build/matter-wrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/matter-attractors@0.1.6/build/matter-attractors.min.js"></script>
+ */}
+
 var canvas = $('#wrapper-canvas').get(0)
 
 var dimensions = {
